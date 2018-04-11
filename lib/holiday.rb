f37b74dc5dae187c0afd1cc0228be30be95e6dd6
Anum Siddiqui <asiddiqui0692@gmail.com>
2018-04-11 05:07:39 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,8 +73,14 @@ end
 end
 
 
+
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  newarray = []
 
+  holiday_hash.each do |season, data|
+    data.each do |holiday, supply|
+      
+  end
 end
